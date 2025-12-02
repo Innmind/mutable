1dfe9ecd7b3393c16be54d85360ac21d1b9ece80
Baptiste Langlade <baptiste.langlade@hey.com>
2025-12-02 15:30:03 +0100

    add Map

---
+++ src/Map.php
@@ -0,0 +1,147 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Mutable;
+
+use Innmind\{
+    Immutable,
+    Immutable\Maybe,
+};
+
+/**
+ * @template K
+ * @template V
+ */
+final class Map
+{
+    /**
+     * @param Immutable\Map<K, V> $map
+     */
+    private function __construct(
+        private Immutable\Map $map,
+    ) {
+    }
+
+    /**
+     * Set a new key/value pair
+     *
+     * @param K $key
+     * @param V $value
+     */
+    public function __invoke($key, $value): void
+    {
+        $this->map = ($this->map)($key, $value);
+    }
+
+    /**
+     * @template A
+     * @template B
+     * @no-named-arguments
+     *
+     * @param list<array{A, B}> $pairs
+     *
+     * @return self<A, B>
+     */
+    public static function of(array ...$pairs): self
+    {
+        return new self(Immutable\Map::of(...$pairs));
+    }
+
+    /**
+     * @return int<0, max>
+     */
+    #[\NoDiscard]
+    public function size(): int
+    {
+        return $this->map->size();
+    }
+
+    /**
+     * Set a new key/value pair
+     *
+     * @param K $key
+     * @param V $value
+     */
+    public function put($key, $value): void
+    {
+        $this->map = ($this->map)($key, $value);
+    }
+
+    /**
+     * Return the element with the given key
+     *
+     * @param K $key
+     *
+     * @return Maybe<V>
+     */
+    #[\NoDiscard]
+    public function get($key): Maybe
+    {
+        return $this->map->get($key);
+    }
+
+    /**
+     * Check if there is an element for the given key
+     *
+     * @param K $key
+     */
+    #[\NoDiscard]
+    public function contains($key): bool
+    {
+        return $this->map->contains($key);
+    }
+
+    /**
+     * Remove all elements from the map
+     */
+    public function clear(): void
+    {
+        $this->map = $this->map->clear();
+    }
+
+    /**
+     * Remove all elements that don't match the predicate
+     *
+     * @param callable(K, V): bool $predicate
+     */
+    public function filter(callable $predicate): void
+    {
+        $this->map = $this->map->filter($predicate);
+    }
+
+    /**
+     * Remove all elements that match the predicate
+     *
+     * @param callable(K, V): bool $predicate
+     */
+    public function exclude(callable $predicate): void
+    {
+        $this->map = $this->map->exclude($predicate);
+    }
+
+    /**
+     * Run the given function for each element of the map
+     *
+     * @param callable(K, V): void $function
+     */
+    public function foreach(callable $function): void
+    {
+        $_ = $this->map->foreach($function);
+    }
+
+    /**
+     * Remove the element with the given key
+     *
+     * @param K $key
+     */
+    public function remove($key): void
+    {
+        $this->map = $this->map->remove($key);
+    }
+
+    #[\NoDiscard]
+    public function empty(): bool
+    {
+        return $this->map->empty();
+    }
+}
